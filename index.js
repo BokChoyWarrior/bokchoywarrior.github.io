2146ddbe78965b7d4d624b663893f3eb2b08da36
Harvey Cambridge <49193507+BokChoyWarrior@users.noreply.github.com>
2024-06-28 21:52:39 +0100

    Lets go!

---
+++ index.js
@@ -0,0 +1,7 @@
+var a = 5;
+var b = 10;
+console.log(a + b);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 5));
---
+++ index.ts
@@ -0,0 +1,10 @@
+const a = 5;
+let b = 10;
+
+console.log(a + b);
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(add(1, 5));
